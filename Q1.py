859d2bf5147e51ce45ddc0e9ca1f365c187b3347
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-08 14:54:13 +0530

    assignment-2

---
+++ Q1.py
@@ -0,0 +1,35 @@
+# Taken cost prise input to the user 
+cost_price = float(input("Enter Cost Price: "))
+
+# Taken selling prise input 
+selling_price = float(input("Enter Selling Price: "))
+
+# checking if selling price is greater than cost price
+if selling_price > cost_price:
+
+   # calculated profit
+    profit = selling_price - cost_price
+
+    # profit percentage formula 
+    profit_percentage = (profit / cost_price) * 100
+
+    # result print
+    print("Profit =", profit)
+    print("Profit Percentage =", profit_percentage,"%")
+
+# checking whether there is a loss or not
+elif cost_price > selling_price:
+
+    # loss nikala
+    loss = cost_price - selling_price
+
+    # calculated loss percentage
+    loss_percentage = (loss / cost_price) * 100
+
+    # result
+    print("Loss =", loss)
+    print("Loss Percentage =", loss_percentage,"%")
+
+# if both are equal 
+else:
+    print("No Profit No Loss")
