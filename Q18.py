3689db9bf62a253777d03c6b49e19ad267ae1abd
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-25 12:08:23 +0530

    Authentication System

---
+++ Q18.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "user1" and password == "pass@123":
+    print("Authentication successful.")
+else:
+    print("Authentication failed.")
