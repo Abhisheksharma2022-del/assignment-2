ea2df98b571386edca70f18970ac57c30c1fbb03
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-25 10:12:18 +0530

    Cricket Stats Analyzer

---
+++ Q2.py
@@ -0,0 +1,11 @@
+p1 = int(input("Enter runs of Player 1: "))
+p2 = int(input("Enter runs of Player 2: "))
+p3 = int(input("Enter runs of Player 3: "))
+p4 = int(input("Enter runs of Player 4: "))
+p5 = int(input("Enter runs of Player 5: "))
+
+total_runs = p1 + p2 + p3 + p4 + p5
+average_runs = total_runs / 5
+
+print("Total Runs =", total_runs)
+print("Average Runs =", average_runs)
